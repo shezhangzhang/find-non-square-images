6f8e5aff7181ac473b2c642b4999b61f84a6d87a
shezhangzhang <ischenkan@outlook.com>
2022-07-11 22:39:25 +0800

    release oneoneeyes v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # find-non-square-images
+
 Find non-square images in folder.
+
+`npm i oneoneeyes -g`
+
+`oneoneeyes path/to/folder`
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import { Command } from "commander";
 import fs from "fs";
 import path from "path";
 import sizeOf from "image-size";
+import ora from "ora";
 const TGA = require("tga");
 
 const program = new Command();
 
-program.description("Find non-square images in folder.");
-program.name("find-images");
+program.description(
+  "Find non-square & size is not 2 to the nth power images in a specific folder."
+);
+program.name("oneoneeyes");
 // program.option("-p, --path <char>", "folder path");
 
 program.parse();
@@ -16,46 +19,56 @@ program.parse();
 const folderPath = program.args[0];
 
 if (fs.existsSync(folderPath)) {
+  const spinner = ora("Processing...").start();
   const result: string[] = [];
+
   fs.readdirSync(path.resolve(folderPath)).forEach((fileName: string) => {
     const lowerCaseFileName = fileName.toLowerCase();
-    const fielPath = path.resolve(folderPath, fileName);
-    if (lowerCaseFileName.endsWith(".png")) {
-      try {
-        const imageInfo = sizeOf(fielPath);
+    const filePath = path.resolve(folderPath, fileName);
 
-        if (
-          imageInfo?.width !== imageInfo?.height ||
-          check2(imageInfo?.width || 0)
-        ) {
-          result.push(fileName);
+    if (
+      lowerCaseFileName.endsWith(".png") ||
+      lowerCaseFileName.endsWith(".tga")
+    ) {
+      try {
+        const imageInfo = getImageInfo(lowerCaseFileName, filePath);
+
+        if (imageInfo?.width !== imageInfo?.height) {
+          result.push(`fileName: ${fileName}, reason: width != height.`);
+        } else if (!check2(imageInfo?.width || 0)) {
+          result.push(
+            `fileName: ${fileName}, reason: size is not 2 to the nth power.`
+          );
         }
       } catch (error) {
         console.log(error);
       }
     }
-
-    if (lowerCaseFileName.endsWith(".tga")) {
-      const tgaImage = new TGA(fs.readFileSync(fielPath));
-
-      if (
-        tgaImage?.width !== tgaImage?.height ||
-        check2(tgaImage?.width || 0)
-      ) {
-        result.push(fileName);
-      }
-    }
   });
 
   if (result.length) {
-    console.log("INCORRECT IMAGES: ", result);
+    spinner.fail(`${result.length} IMAGES FAILED：`);
+    console.log(result);
   } else {
-    console.log("CONGRATULATIONS!");
+    spinner.succeed("ALL PASSED, CONGRATULATIONS!");
   }
 } else {
   console.log("ERROR: PATH IS NOT EXISTS.");
 }
 
+function getImageInfo(fileName: string, filePath: string) {
+  let imageInfo: any;
+
+  if (fileName.endsWith("png")) {
+    imageInfo = sizeOf(filePath);
+  } else {
+    imageInfo = new TGA(fs.readFileSync(filePath));
+  }
+
+  return imageInfo;
+}
+
+// check num is '2 to the nth power'
 function check2(num: number) {
-  return num > 0 && (num & (num - 1)) == 0;
+  return num > 0 && (num & (num - 1)) === 0;
 }
